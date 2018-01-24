ffc5ddc461e22d856a3d590ac462fbc8b3485a13
Pete Gore <pierre.mihawk@gmail.com>
2018-01-24 07:59:52 +0100

    Add Webpack and VueJS support with Symfony/Webpack-Encore package

---
+++ app/config/webpack/webpack.config.js
@@ -0,0 +1,21 @@
+// config/webpack/webpack.config.js
+
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+    // See https://symfony.com/doc/current/frontend/encore/simple-example.html
+    .setOutputPath('public/build/')
+    .setPublicPath('/build')
+    .addEntry('app', './assets/js/app.js')
+    .enableSassLoader()
+    .autoProvidejQuery()
+    .enableSourceMaps(!Encore.isProduction())
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+
+    // VueJS
+    .enableVueLoader()
+;
+
+// export the final configuration
+module.exports = Encore.getWebpackConfig();
